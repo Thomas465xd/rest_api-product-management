4d5e6bea6f565ea20f4694d7470adaf20b0d0d3c
Thomas_465 <160943727+Thomas465xd@users.noreply.github.com>
2024-10-27 15:04:22 -0300

    *Added jest and supertest for testing the project

---
+++ src/__tests__/server.test.ts
@@ -0,0 +1,9 @@
+describe("Nuestro Primer Test", () => {
+    test("Debe revisar que 1 + 1 sean 2", () => {
+        expect(1 + 1).toBe(2)
+    })
+
+    test("Debe revisar que 1 + 1 no sean 3", () => {
+        expect(1 + 1).not.toBe(3)
+    })
+})
