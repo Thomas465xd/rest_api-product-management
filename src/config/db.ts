77b33712b33dc6f86c1b3bfa1f39aee01c59d966
Thomas_465 <160943727+Thomas465xd@users.noreply.github.com>
2024-10-28 19:28:37 -0300

    Again

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,19 +1,18 @@
 import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv";
-import path from "path";
-import Product from "../models/Product.model.js";
 
 dotenv.config();
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-const modelsPath = path.join(__dirname, "../models");
-
 const db = new Sequelize(process.env.DB_URL!, {
     dialect: 'postgres',
-    logging: false,
-    models: [Product]
+    logging: false
 });
 
-db.addModels([modelsPath]);
+async function initializeModels() {
+    const { default: Product } = await import("../models/Product.model.js");
+    db.addModels([Product]);
+}
+
+initializeModels().catch(console.error);
 
 export default db;
